8da48f690bc7f35e9bc9845ad43c1ab61ee8c43b
Wesley rolim <rolim.r@icloud.com>
2020-06-23 00:56:11 -0300

    Secao 5 Finalizada

---
+++ Desafio5_Orientado_A_Objetos/Desafio5_Orientado_A_Objetos/ContaBancaria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Desafio5_Orientado_A_Objetos
+{
+    class ContaBancaria
+    {
+
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+        public int Ncc { get; set; }
+
+        public ContaBancaria(int ncc, string nome)
+        {
+            Ncc = ncc;
+            Nome = nome;
+        }
+
+        public ContaBancaria(int ncc, string nome, double saldo)
+        {
+            Ncc = ncc;
+            Nome = nome;
+            Saldo = saldo;
+        }
+
+        public double Deposito(double valor)
+        {
+            return this.Saldo += valor;
+        }
+
+        public double Saque(double valor)
+        {
+            double Taxa = 5;
+            this.Saldo -= Taxa;
+            return this.Saldo -= valor;
+        }
+
+        public void DepositoInicial(string resposta)
+        {
+            if (resposta == "s".ToLower())
+            {
+                Console.Write("\n\nEntre com o valor de deposito inicial: ");
+                double valor = double.Parse(Console.ReadLine());
+
+                this.Deposito(valor);
+            }
+        }
+/*
+        public override string ToString()
+        {
+            return "\n\nDados da conta: \nConta " + this.Ncc + ", Titular: " + this.Nome + ", Saldo: $ " + this.Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+      */
+   
+        public void AtualizarDados()
+        {
+            Console.WriteLine("Conta " + this.Ncc + ", Titular: " + this.Nome + ", Saldo: $ " + this.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+        }
+  
+    }
+}
---
+++ Desafio5_Orientado_A_Objetos/Desafio5_Orientado_A_Objetos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Desafio5_Orientado_A_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Entre com o numero da conta: ");
+            int nc = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            ContaBancaria cb = new ContaBancaria(nc, titular);
+
+            Console.Write("Havera deposito inicial (s/n)? ");
+            string resp = Console.ReadLine();
+            cb.Saldo = 0;    
+            cb.DepositoInicial(resp);
+            Console.WriteLine("\n\nDados da Conta:");
+            cb.AtualizarDados();
+
+            Console.Write("\n\nEntre com um valor para deposito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            cb.Deposito(valorDeposito);
+
+            cb.AtualizarDados();
+
+            Console.Write("\n\nEntre com um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            cb.Saque(valorSaque);
+            cb.AtualizarDados();
+        }
+    }
+}
