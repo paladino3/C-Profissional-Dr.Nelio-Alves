e32c4b1eabcac0b158acd3aca035b2bfb7b199c5
Wesley rolim <rolim.r@icloud.com>
2020-06-25 21:39:32 -0300

    feito exercicio da calculadora orientado

---
+++ Ex16_Orientado_a_Objetos/Ex16_Orientado_a_Objetos/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex16_Orientado_a_Objetos
+{
+    class Calculadora
+    {
+        public static int Soma(params int[] numero)
+        {
+            int soma = 0;
+            for (int i = 0; i < numero.Length; i++)
+            {
+                soma += numero[i];
+            }
+            return soma;
+        }
+
+    }
+}
---
+++ Ex16_Orientado_a_Objetos/Ex16_Orientado_a_Objetos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex16_Orientado_a_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma(3, 4);
+            int s2 = Calculadora.Soma(3, 4, 74, 4554, 0);
+            int s3 = Calculadora.Soma(3, 4, 47, 4, 44, 1, 74);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
