fb0e0a1d347c72c811179045bcb3e786cffb7106
Wesley rolim <rolim.r@icloud.com>
2020-06-30 23:20:10 -0300

    DataTime secao 07

---
+++ Ex20/Ex20/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Ex20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* DateTime d1 = DateTime.Now;
+             Console.WriteLine(d1);
+             Console.WriteLine(d1.Ticks);*/
+            /*
+                        DateTime d2 = new DateTime(2020, 06, 30);
+                        DateTime d3 = new DateTime(2020, 06, 30, 20, 11, 55);
+                        DateTime d4 = new DateTime(2020, 06, 30, 20, 11, 55, 300);
+
+                        DateTime d5 = DateTime.Now;
+                        DateTime d6 = DateTime.UtcNow;
+                        DateTime d7 = DateTime.Today;
+
+                        Console.WriteLine(d2);
+                        Console.WriteLine(d3);
+                        Console.WriteLine(d4);
+
+
+                        Console.WriteLine("_________________________");
+                        Console.WriteLine(d5);
+                        Console.WriteLine(d6);
+                        Console.WriteLine(d7); 
+                        */
+
+            DateTime d1 = DateTime.Parse("2000-08-15");
+            DateTime d10 = DateTime.Parse("2000-08-15 13:05:52");
+            DateTime d2 = DateTime.Parse("06/30/2020");
+            DateTime d3 = DateTime.Parse("08/15/2000");
+
+
+            DateTime d4 = DateTime.ParseExact("15-08-2020", "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            DateTime d5 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d10);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+        }
+    }
+}
---
+++ Ex21/Ex21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TimeSpan");
+            /*
+                        TimeSpan t1 = new TimeSpan(0, 1, 20);
+                        TimeSpan t2 = new TimeSpan(800000000L);
+                        TimeSpan t3 = new TimeSpan(2, 11, 20);
+                        TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+                        TimeSpan t5 = new TimeSpan(1, 3, 5, 24, 052);
+
+                        Console.WriteLine(t1);
+                        Console.WriteLine(t1.Ticks);
+                        Console.WriteLine(t2);
+                        Console.WriteLine(t3);
+                        Console.WriteLine(t4);
+                        Console.WriteLine(t5);
+            */
+
+
+            TimeSpan t1 = TimeSpan.FromDays(1.5);
+            TimeSpan t2 = TimeSpan.FromHours(1.5);
+            TimeSpan t3 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t4 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t5 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t6 = TimeSpan.FromTicks(800000000L);
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+        }
+    }
+}
