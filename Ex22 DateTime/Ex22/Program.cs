abe28ea45d2835f2ba5cf33ef8dd7847b8f28478
Wesley rolim <rolim.r@icloud.com>
2020-07-01 22:04:28 -0300

    finalizacao da secao 07

---
+++ Ex22 DateTime/Ex22/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Ex22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  DateTime data = new DateTime(2020, 7, 1, 18, 02, 50, 770);
+
+              Console.WriteLine(data);
+
+              Console.WriteLine("1) Data: "+data.Date);
+              Console.WriteLine("2) Dia: "+data.Day);
+              Console.WriteLine("3) DiaDaSemana: "+data.DayOfWeek);
+              Console.WriteLine("4) DiaDoAno: "+data.DayOfYear);
+              Console.WriteLine("5) Horas: "+data.Hour);
+              Console.WriteLine("6) Kind: "+data.Kind);
+              Console.WriteLine("7) Minutos: "+data.Minute);
+              Console.WriteLine("9) Segundos: "+data.Second);
+              Console.WriteLine("8) Milesegundos: "+data.Millisecond);
+              Console.WriteLine("9) Ticks: "+data.Ticks);
+              Console.WriteLine("10) TimeOfDay: "+data.TimeOfDay);
+              Console.WriteLine("11) Ano: "+data.Year);
+              Console.WriteLine("12) Mes: "+data.Month);
+  */
+
+            /*
+                        DateTime data = new DateTime(2020, 7, 1, 18, 02, 50, 770);
+
+                        Console.WriteLine(data.ToLongDateString());
+                        Console.WriteLine(data.ToLongTimeString());
+                        Console.WriteLine(data.ToShortTimeString());
+
+                        string s1 = data.ToString();
+                        string s2 = data.ToString("dd-MM-yyyy HH:mm:ss");
+                        string s3 = data.ToString("dd-MM-yyyy HH:mm:ss.fff");
+
+                        Console.WriteLine(s1);
+                        Console.WriteLine(s2);
+                        Console.WriteLine(s3);
+
+
+                */
+
+            DateTime data = new DateTime(2020, 7, 1, 18, 02, 50, 770);
+
+            DateTime d2 = data.AddHours(2);
+            DateTime d3 = data.AddMinutes(3);
+
+            Console.WriteLine(data);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+            DateTime dataAtual = DateTime.Now; // data atual
+            Console.WriteLine("____________________________________________________________");
+            Console.WriteLine(dataAtual.ToLongDateString() + dataAtual.ToLongTimeString());
+            Console.WriteLine(dataAtual.ToString());
+
+            DateTime vencimento = dataAtual.AddDays(7);
+            Console.WriteLine("Vencimento para: "+vencimento.ToString("dd/MM/yyyy"));
+
+
+            Console.WriteLine("_________________________________________________");
+            TimeSpan tempo = vencimento.Subtract(dataAtual);
+            Console.WriteLine("Dias, Horas, Minutos... "+tempo);
+            Console.WriteLine("Numeros de dias: "+tempo.ToString("dd"));
+        }
+
+    }
+}
---
+++ Ex23 TimeSpanPropriedades/Ex23 TimeSpanPropriedades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex23_TimeSpanPropriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                        TimeSpan t1 = TimeSpan.MaxValue;
+                        TimeSpan t2 = TimeSpan.MinValue;
+                        TimeSpan t3 = TimeSpan.Zero;
+
+                        Console.WriteLine(t1);
+                        Console.WriteLine(t2);
+                        Console.WriteLine(t3);
+            */
+
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 05);
+
+            TimeSpan soma = t1.Add(t2);
+            TimeSpan dif = t1.Subtract(t2);
+            TimeSpan mult = t1.Multiply(2.0);
+            TimeSpan divide = t1.Divide(2.0);
+
+            Console.WriteLine("Soma de " + t1 + " + " + t2 + " = " + soma);
+            Console.WriteLine("Diferenca de " + t1 + " - " + t2 + " = " + dif);
+            Console.WriteLine("O valor de " + t1 + " x2 = " + mult);
+            Console.WriteLine("O valor de " + t1 + " /2 = " + divide);
+
+
+
+
+        }
+    }
+}
