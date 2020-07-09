ff93a0258092a74a7fd16ed964debef483ac8f94
Wesley rolim <rolim.r@icloud.com>
2020-07-09 01:02:00 -0300

    Ex34 Criando execoes personalizadas parte 1

---
+++ Ex33 Estrutura try-catch/Ex33/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Ex33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tratamento de exececoes
+
+            try
+            {
+                int number1 = int.Parse(Console.ReadLine());
+                int number2 = int.Parse(Console.ReadLine());
+
+                int result = (number1 / number2);
+
+                Console.WriteLine(result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro de sintaxe, foi capturado uma letra ao invez de numero " + e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Erro nao nao pode dividir por zero! " + e.Message);
+            }
+            finally
+            {
+                // a palavra finally, realiza uma açao independente se funcionou o cod, ou deu erros!
+                Console.WriteLine("\n\niNDEPENDENTE DO QUE ACONTEÇA, ESSA MENSSAGEM VAI APARECER! ");
+            }
+
+        }
+    }
+}
---
+++ Ex34 Criando exceçoes personalizadas - PARTE 1/Ex34/Entities/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex34.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            //TimeSpan que é a duração de algo
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber +
+                ", Check-in: " +
+                CheckIn.ToString("dd/MM/yyyy") +
+                ", Check-out: " + CheckOut.ToString("dd/MM/yyyy") +
+                ", " + Duration() +
+                " nights";
+        }
+    }
+}
