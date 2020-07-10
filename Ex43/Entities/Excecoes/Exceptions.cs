c42188ee4732adc862ee0238af6f49ec617c9867
Wesley rolim <rolim.r@icloud.com>
2020-07-09 23:51:35 -0300

    Resolvendo o ex43 secao 14

---
+++ Ex43/Entities/Excecoes/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex43.Entities.Excecoes
+{
+    class Exceptions : SystemException
+    {
+
+        public Exceptions(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ Ex43/Program.cs
@@ -0,0 +1,41 @@
+﻿using Ex43.Entities;
+using System;
+using System.Globalization;
+
+namespace Ex43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Aluguel de carros dados");
+                Console.Write("Modelo do carro: ");
+                string modelo = Console.ReadLine();
+                Console.Write("Data de retirada (dd/mm/yyyy hh:ss): ");
+                DateTime retirada = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data de devolucao (dd/mm/yyyy hh:ss): ");
+                DateTime devolucao = DateTime.Parse(Console.ReadLine());
+                Console.Write("Entre com o preco por hora: ");
+                double precoHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Entre com o preco por dia: ");
+                double precoDia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Locadora locadora = new Locadora(modelo, retirada, devolucao, precoHora, precoDia);
+
+                locadora.Duracao(retirada, devolucao, precoHora, precoDia);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na digitacao");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
