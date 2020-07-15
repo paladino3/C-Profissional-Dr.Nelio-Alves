0349ae547c175a314388dc32dc3406fed585678a
Wesley rolim <rolim.r@icloud.com>
2020-07-14 23:34:05 -0300

    Finalizacao da secao 14

---
+++ Ex46 Interface IComparable 3/Entities/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Claims;
+using System.Text;
+
+namespace ordernar.Entities
+{
+    class Funcionario : IComparable
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(string Cv_func)
+        {
+            string[] vector = Cv_func.Split(',');
+            Name = vector[0];
+            Id = int.Parse(vector[1]);
+            Salary = double.Parse(vector[2], CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Funcionario other = obj as Funcionario;
+
+            //  return Name.CompareTo(other.Name);
+            //  return Id.CompareTo(other.Id);
+             return Salary.CompareTo(other.Salary);
+        }
+
+
+        public override string ToString()
+        {
+            return "Name: " + Name +
+                "Id: " + Id +
+                "Salary: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Ex46 Interface IComparable 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using ordernar.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ordernar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\rolim\Desktop\Curso C# Profissional Dr.Nelio Alves\Seçao 14 Interfaces\Interface_IComparable\func.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+
+                    foreach(Funcionario func in list)
+                    {
+                        Console.WriteLine(func);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred.");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
