68ec6fa1dfcf4bcb2a7f112330c58cfd00d8fe71
Wesley rolim <rolim.r@icloud.com>
2020-07-15 21:34:58 -0300

    Ex48 Restricoes a Generics

---
+++ Ex48 Restriçoes de generics/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace Ex48.Entities
+{
+    class Product : IComparable
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: is not a product");
+            }
+            Product other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+        }
+
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Ex48 Restriçoes de generics/Services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex48.Services
+{
+    class CalculationService
+    {
+        public T Max<T>(List<T> lista) where T : IComparable
+        {
+            if (lista.Count == 0)
+            {
+                throw new ArgumentException("A lista nao pode ser vazia");
+            }
+
+            T max = lista[0]; // primeiro elemento da lista
+            for (int i = 1; i < lista.Count; i++)
+            {
+                if (lista[i].CompareTo(max) > 0)
+                {
+                    max = lista[i];
+                }
+            }
+            return max;
+        }
+    }
+}
