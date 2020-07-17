a369170629d2d52ae2f404e5591efbbfd46f35cd
Wesley rolim <rolim.r@icloud.com>
2020-07-17 16:04:16 -0300

    Exercicio proposto sobre conjuntos feito!

---
+++ Ex54 Exercício proposto sobre conjuntos/Entities/Course.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex54.Entities
+{
+    class Course
+    {
+        public int Id { get; set; }
+        public string Student { get; set; }
+
+        HashSet<Course> courses = new HashSet<Course>();
+
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Course))
+            {
+                return false;
+            }
+
+            Course other = obj as Course;
+            return Id.Equals(other.Id);
+        }
+
+
+        public void NewCourse(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                courses.Add(new Course { Id = x });
+            }
+        }
+
+        public void Count()
+        {
+            Console.WriteLine("Total studants: " + courses.Count);
+        }
+
+    }
+}
---
+++ Ex54 Exercício proposto sobre conjuntos/Program.cs
@@ -0,0 +1,35 @@
+﻿using Ex54.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Ex54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+
+            Console.Write("How many studants for course A? ");
+            int a = int.Parse(Console.ReadLine());
+            course.NewCourse(a);
+
+
+
+            Console.Write("How many studants for course B? ");
+            int b = int.Parse(Console.ReadLine());
+            course.NewCourse(b);
+
+
+            Console.Write("How many studants for course C? ");
+            int c = int.Parse(Console.ReadLine());
+            course.NewCourse(c);
+
+
+            course.Count();
+
+
+        }
+    }
+}
