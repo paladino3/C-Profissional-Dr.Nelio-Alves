3c204947fa1da600201d6c016e14e50719532e30
Wesley rolim <rolim.r@icloud.com>
2020-07-20 19:58:42 -0300

    Secao 17 ex 57 Uma experiência com Comparison

---
+++ Ex57 Uma experiencia com Comparison/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Ex57.Entities
+{
+    class Product
+
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+/*
+        public int CompareTo([AllowNull] Product obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Um erro relativo");
+            }
+            Product other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+        }
+*/
+        public override string ToString()
+        {
+            return Name + ", " + Price;
+        }
+    }
+}
---
+++ Ex57 Uma experiencia com Comparison/Program.cs
@@ -0,0 +1,41 @@
+﻿using Ex57.Entities;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ex57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("PC", 3200.00));
+            list.Add(new Product("Notebook", 2900.00));
+            list.Add(new Product("Monitor", 1900.00));
+
+
+            //Comparison<Product> comparacao = CompareProducts;
+            //    list.Sort(CompareProducts);
+            //    list.Sort(comparacao);
+
+
+            //expressao lambda
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product e in list)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        //metodo fora do escopo do void Main
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
+    }
+}
