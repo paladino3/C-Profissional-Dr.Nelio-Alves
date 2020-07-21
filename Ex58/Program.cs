0f4673219de52c29595c6389b3b6934474ae4a2e
Wesley rolim <rolim.r@icloud.com>
2020-07-20 23:52:44 -0300

    Secao 17 ex 58 feito

---
+++ Ex58/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ex58.Services;
+using System;
+
+namespace Ex58
+{
+    delegate double TesteCalculos(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            TesteCalculos sum = CalculationServices.Sum;
+            TesteCalculos max = CalculationServices.Max;
+           // TesteCalculos square = CalculationServices.Square;
+
+            
+            double result = sum.Invoke(a, b);
+            double result1 = max(a, b);
+            //double result2 = square(a); //da erro porque o delagate espera dois parametros
+
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+        }
+    }
+}
