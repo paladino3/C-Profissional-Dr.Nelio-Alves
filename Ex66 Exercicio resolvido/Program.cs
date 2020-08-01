de1df1de309d89e8552f70bea91fe7cc769351b9
Wesley rolim <rolim.r@icloud.com>
2020-07-31 23:56:59 -0300

    Ex66 cap 17 ex resolvido

--- Ex66 Exercicio resolvido/Program.cs
+++ Ex66 Exercicio resolvido/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Ex66.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace Ex66
 {
@@ -6,7 +11,35 @@ namespace Ex66
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+
+                    list.Add(new Product(name, price));
+                }
+
+
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price =  " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(x => x.Price < avg).OrderByDescending(x => x.Name).Select(x => x.Name);
+            foreach(string item in names)
+            {
+
+            Console.WriteLine(item);
+            }
+
         }
     }
 }
